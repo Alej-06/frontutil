0fd39caac0dd3e925617bc4b1433cd52ce875b39
Alejandro Pavon <alejandropm06@gmail.com>
2025-11-17 11:50:47 +0100

    Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/shared/home/home';
+import { PlistBlogPavon } from './component/shared/feature/blog/plist/plist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'blog', component: PlistBlogPavon}
+];
