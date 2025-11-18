504c76e509408f8fecb2a75b5d426d6a5ba27339
Alejandro Pavon <alejandropm06@gmail.com>
2025-11-18 12:54:32 +0100

    Botonera 6.0 El remaster

--- src/app/service/blog.ts
+++ src/app/service/blog.ts
@@ -12,8 +12,8 @@ export class BlogService {
 
   constructor(private oHttp: HttpClient){}
   
-  getPage(page: number, rpp:number): Observable<IPage<Blog>> {
-    return this.oHttp.get<IPage<Blog>>(serverURL + `/blog?page=${page}&${rpp}`) //DEVUELVE OBSERBABLE, NO SABERLO DEPRIME A RAFA
+  getPage(page: number, rpp: number): Observable<IPage<Blog>> {
+    return this.oHttp.get<IPage<Blog>>(serverURL + `/blog?page=${page}&size=${rpp}`);//DEVUELVE OBSERBABLE, NO SABERLO DEPRIME A RAFA
   }
 
 }
